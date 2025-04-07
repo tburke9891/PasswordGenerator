8ead1674ffec65620f9fc1f63ce5db441db2eb93
mebannon <mebannon@my.waketech.edu>
2025-04-07 19:51:56 -0400

    Updated to generate psuedo-random seed

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -7,14 +7,19 @@
 #
 ######################################################################
 
+import os     # Imports Operating System
+
 # Character sets for password generator
 capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lowers = "abcdefghijklmnopqrstuvqxyz"
 numbers = "0123456789"
-special_chars = "!@#$%^&*()_-=[]{}|\;:',<.>/?`~"
+special_chars = "!@#$%^&*()_-=[]{}|;:',<.>/?`~"
 #length = 0
 all_chars = capitals + lowers + numbers + special_chars
 password = ""
+t = os.times()
+seed = int((t.user + t.system + t.elapsed) * 8675309)
+seeds = []
 
 # Ask user how long they want their generated password to be.
 length = int(input("How long would you like your password? (Min:8 - Max:26) "))
@@ -25,15 +30,22 @@ elif length < 26:
 else:
     print("Password too long, how the fuck are you supposed to remember that?!")
 
-# x = ord("A")
-# print(x)
-random_keys = input("Press random keys!!! ")
+seed = seed % length
 
 for i in range(length):
+    bag_of_seeds = (seed * (i + 2300000) + i * i) ^ (seed * (i % 5))
+    number = bag_of_seeds % length
+    seeds.append(number)
+else:
+    print(seeds)
+
+# x = ord("A") - ord will get the ASCII value of "A"
+
+random_keys = input("Press random keys!!! ")
+for i in range(length):
     rk = random_keys[i % len(random_keys)]
-    pseudo_random = (ord(rk) + i * 17) % len(all_chars)
+    pseudo_random = (ord(rk) + i * seeds[i]) % len(all_chars)
     password += all_chars[pseudo_random]
 else:
     print(f"Your random password is: {password}")
 
-#Tp1[>Rdz@<
